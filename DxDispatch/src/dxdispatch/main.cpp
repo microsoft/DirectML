a45c5a431061a2158e709692d44de9922332974c
Justin Stoecker <justoeck@microsoft.com>
2022-10-24 14:04:52 -0700

    DxDispatch: option to show more info for loaded dependencies (#312)

--- DxDispatch/src/dxdispatch/main.cpp
+++ DxDispatch/src/dxdispatch/main.cpp
@@ -9,15 +9,9 @@
 #endif
 #include "Executor.h"
 #include "CommandLineArgs.h"
+#include "ModuleInfo.h"
 
 using Microsoft::WRL::ComPtr;
-
-#if !defined(_GAMING_XBOX) && defined(WIN32)
-// Needed for DX12 agility SDK. Xbox uses DX12.x from the GDK.
-// https://devblogs.microsoft.com/directx/gettingstarted-dx12agility/
-extern "C" { __declspec(dllexport) extern const UINT D3D12SDKVersion = DIRECT3D_AGILITY_SDK_VERSION; }
-extern "C" { __declspec(dllexport) extern const char* D3D12SDKPath = DIRECT3D_AGILITY_SDK_PATH; }
-#endif
 
 int main(int argc, char** argv)
 {
@@ -32,12 +26,6 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    if (args.PrintHelp())
-    {
-        LogInfo(args.HelpText());
-        return 0;
-    }
-
     // Needs to be constructed *before* D3D12 device. A warning is printed if DXCore.dll is loaded first,
     // even though the D3D12Device isn't created yet, so we create the capture helper first to avoid this
     // message.
@@ -46,77 +34,96 @@ int main(int argc, char** argv)
     auto d3dModule = std::make_shared<D3d12Module>();
     auto dmlModule = std::make_shared<DmlModule>();
 
+    if (args.PrintHelp())
+    {
+        LogInfo(args.HelpText());
+        return 0;
+    }
+
+    if (args.ShowDependencies())
+    {
+#if defined(_WIN32) && !defined(_GAMING_XBOX)
+        // D3D12.dll lazily loads D3D12Core.dll. Calling any exported function forces D3D12Core.dll to load
+        // so its version can be printed, and GetDebugInterface is inexpensive.
+        Microsoft::WRL::ComPtr<ID3D12Debug> debug;
+        d3dModule->GetDebugInterface(IID_PPV_ARGS(&debug));
+#endif
+
+        PrintDependencies();
+    }
+
     if (args.ShowAdapters())
     {
         for (auto& adapter : Adapter::GetAll(dxCoreModule))
         {
             LogInfo(adapter.GetDetailedDescription() + "\n");
         }
-
-        return 0;
     }
 
-    std::shared_ptr<Device> device;
-    try
+    if (!args.ModelPath().empty())
     {
-        Adapter adapter = Adapter::Select(dxCoreModule, args.AdapterSubstring());
-        device = std::make_shared<Device>(
-            adapter.GetAdapter(), 
-            args.DebugLayersEnabled(), 
-            args.CommandListType(),
-            pixCaptureHelper,
-            d3dModule,
-            dmlModule
-        );
-        LogInfo(fmt::format("Running on '{}'", adapter.GetDescription()));
-    }
-    catch (std::exception& e)
-    {
-        LogError(fmt::format("Failed to create a device: {}", e.what()));
-        return 1;
-    }
-
-    Model model;
-    try
-    {
-        if (args.ModelPath().extension() == ".json")
+        std::shared_ptr<Device> device;
+        try
         {
-            model = JsonParsers::ParseModel(args.ModelPath());
-        }
-        else if (args.ModelPath().extension() == ".onnx")
-        {
-#ifdef ONNXRUNTIME_NONE
-            throw std::invalid_argument("ONNX dispatchables require ONNX Runtime");
-#else
-            model = OnnxParsers::ParseModel(
-                device->DML(), 
-                device->GetCommandQueue(),
-                args.ModelPath(), 
-                args.GetOnnxFreeDimensionNameOverrides(),
-                args.GetOnnxFreeDimensionDenotationOverrides()
+            Adapter adapter = Adapter::Select(dxCoreModule, args.AdapterSubstring());
+            device = std::make_shared<Device>(
+                adapter.GetAdapter(), 
+                args.DebugLayersEnabled(), 
+                args.CommandListType(),
+                pixCaptureHelper,
+                d3dModule,
+                dmlModule
             );
-#endif
+            LogInfo(fmt::format("Running on '{}'", adapter.GetDescription()));
         }
-        else
+        catch (std::exception& e)
         {
-            throw std::invalid_argument("Expected a .json or .onnx file");
+            LogError(fmt::format("Failed to create a device: {}", e.what()));
+            return 1;
         }
-    }
-    catch (std::exception& e)
-    {
-        LogError(fmt::format("Failed to parse the model: {}", e.what()));
-        return 1;
-    }
 
-    try
-    {
-        Executor executor{model, device, args};
-        executor.Run();
-    }
-    catch (std::exception& e)
-    {
-        LogError(fmt::format("Failed to execute the model: {}", e.what()));
-        return 1;
+        Model model;
+        try
+        {
+            if (args.ModelPath().extension() == ".json")
+            {
+                model = JsonParsers::ParseModel(args.ModelPath());
+            }
+            else if (args.ModelPath().extension() == ".onnx")
+            {
+    #ifdef ONNXRUNTIME_NONE
+                throw std::invalid_argument("ONNX dispatchables require ONNX Runtime");
+    #else
+                model = OnnxParsers::ParseModel(
+                    device->DML(), 
+                    device->GetCommandQueue(),
+                    args.ModelPath(), 
+                    args.GetOnnxFreeDimensionNameOverrides(),
+                    args.GetOnnxFreeDimensionDenotationOverrides()
+                );
+    #endif
+            }
+            else
+            {
+                throw std::invalid_argument("Expected a .json or .onnx file");
+            }
+        }
+        catch (std::exception& e)
+        {
+            LogError(fmt::format("Failed to parse the model: {}", e.what()));
+            return 1;
+        }
+
+        try
+        {
+            Executor executor{model, device, args};
+            executor.Run();
+        }
+        catch (std::exception& e)
+        {
+            LogError(fmt::format("Failed to execute the model: {}", e.what()));
+            return 1;
+        }
     }
 
     // Ensure remaining D3D references are released before the D3D module is released.
