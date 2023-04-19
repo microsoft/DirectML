e3eb8f78b041a24fe69698e4de56f688d2cb59bf
Justin Stoecker <justoeck@microsoft.com>
2023-04-18 23:51:46 -0700

    info

--- DxDispatch/src/dxdispatch/Device.cpp
+++ DxDispatch/src/dxdispatch/Device.cpp
@@ -417,20 +417,28 @@ IDxcCompiler3* Device::GetDxcCompiler()
 
 void Device::ClearShaderCaches()
 {
-    constexpr D3D12_SHADER_CACHE_KIND_FLAGS cacheKinds[] = 
+    struct
     {
-        D3D12_SHADER_CACHE_KIND_FLAG_IMPLICIT_D3D_CACHE_FOR_DRIVER,
-        D3D12_SHADER_CACHE_KIND_FLAG_IMPLICIT_D3D_CONVERSIONS,
-        D3D12_SHADER_CACHE_KIND_FLAG_IMPLICIT_DRIVER_MANAGED,
-        D3D12_SHADER_CACHE_KIND_FLAG_APPLICATION_MANAGED,
+        D3D12_SHADER_CACHE_KIND_FLAGS kind;
+        const char* name;
+    } caches[] =
+    {
+        { D3D12_SHADER_CACHE_KIND_FLAG_IMPLICIT_D3D_CACHE_FOR_DRIVER, "D3D12_SHADER_CACHE_KIND_FLAG_IMPLICIT_D3D_CACHE_FOR_DRIVER" },
+        { D3D12_SHADER_CACHE_KIND_FLAG_IMPLICIT_D3D_CONVERSIONS, "D3D12_SHADER_CACHE_KIND_FLAG_IMPLICIT_D3D_CONVERSIONS" },
+        { D3D12_SHADER_CACHE_KIND_FLAG_IMPLICIT_DRIVER_MANAGED, "D3D12_SHADER_CACHE_KIND_FLAG_IMPLICIT_DRIVER_MANAGED" },
+        { D3D12_SHADER_CACHE_KIND_FLAG_APPLICATION_MANAGED, "D3D12_SHADER_CACHE_KIND_FLAG_APPLICATION_MANAGED" },
     };
 
-    for (auto cacheKind : cacheKinds)
+    for (auto cache : caches)
     {
-        auto hr = m_d3d->ShaderCacheControl(cacheKind, D3D12_SHADER_CACHE_CONTROL_FLAG_CLEAR);
+        auto hr = m_d3d->ShaderCacheControl(cache.kind, D3D12_SHADER_CACHE_CONTROL_FLAG_CLEAR);
         if (FAILED(hr))
         {
-            LogInfo(fmt::format("Clearing cache {} failed with HRESULT {}", cacheKind, hr));
+            LogInfo(fmt::format("Clearing {} failed. Do you have developer mode enabled?", cache.name));
+        }
+        else
+        {
+            LogInfo(fmt::format("Clearing {} succeeded.", cache.name));
         }
     }
 }
