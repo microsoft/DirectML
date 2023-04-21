ebed0688f094abaacbcf2e339e7e04da40e718d9
Justin Stoecker <justoeck@microsoft.com>
2023-04-20 23:58:05 -0700

    asdf

--- DxDispatch/tools/RunTest.ps1
+++ DxDispatch/tools/RunTest.ps1
@@ -5,6 +5,7 @@ param(
     $w = 640,
     $layout = 'nhwc',
     $dataType = 'FLOAT32',
+    [switch]$useDml,
     [parameter(ValueFromRemainingArguments=$true, Position=0)][string[]]$ExtraArgs
 )
 
@@ -19,33 +20,94 @@ else
     $strides = "[$($h*$w*$c), 1, $($w*$c), $c]"
 }
 
-$template = @"
+if ($useDml)
 {
-    "resources": 
+    $template = @"
     {
-        "A": { "initialValuesDataType": "$dataType", "initialValues": { "valueCount": $elementCount, "value": 0 } },
-        "B": { "initialValuesDataType": "$dataType", "initialValues": { "valueCount": $elementCount, "value": 0 } },
-        "Out": { "initialValuesDataType": "$dataType", "initialValues": { "valueCount": $elementCount, "value": 0 } }
-    },
-
-    "dispatchables": 
-    {
-        "add": 
+        "resources": 
         {
-            "type": "DML_OPERATOR_ELEMENT_WISE_ADD",
-            "desc": 
+            "A": { "initialValuesDataType": "$dataType", "initialValues": { "valueCount": $elementCount, "value": 0 } },
+            "B": { "initialValuesDataType": "$dataType", "initialValues": { "valueCount": $elementCount, "value": 0 } },
+            "Out": { "initialValuesDataType": "$dataType", "initialValues": { "valueCount": $elementCount, "value": 0 } }
+        },
+    
+        "dispatchables": 
+        {
+            "add": 
             {
-                "ATensor": { "DataType": "$dataType", "Sizes": [$n,$c,$h,$w], "Strides": $strides },
-                "BTensor": { "DataType": "$dataType", "Sizes": [$n,$c,$h,$w], "Strides": $strides },
-                "OutputTensor": { "DataType": "$dataType", "Sizes": [$n,$c,$h,$w], "Strides": $strides }
+                "type": "DML_OPERATOR_ELEMENT_WISE_ADD",
+                "desc": 
+                {
+                    "ATensor": { "DataType": "$dataType", "Sizes": [$n,$c,$h,$w], "Strides": $strides },
+                    "BTensor": { "DataType": "$dataType", "Sizes": [$n,$c,$h,$w], "Strides": $strides },
+                    "OutputTensor": { "DataType": "$dataType", "Sizes": [$n,$c,$h,$w], "Strides": $strides }
+                }
             }
-        }
-    },
-
-    "commands": [ { "type": "dispatch", "dispatchable": "add", "bindings": { "ATensor": "A", "BTensor": "B", "OutputTensor": "Out" } } ]
-}
+        },
+    
+        "commands": [ { "type": "dispatch", "dispatchable": "add", "bindings": { "ATensor": "A", "BTensor": "B", "OutputTensor": "Out" } } ]
+    }
 "@
+}
+else
+{
+    $numThreads = 128
+    $hlslType = if ($dataType -eq "FLOAT32") { "float" } else { "float16_t" }
+    $groupsX = [Math]::Ceiling($elementCount / $numThreads)
+    $threadGroupCount = "[$groupsX, 1, 1]"
+
+    $hlslPath = (Resolve-Path "$PSScriptRoot/../models/hlsl_add.hlsl") -replace '\\','/'
+
+    $template = @"
+    {
+        "resources": 
+        {
+            "A": { "initialValuesDataType": "$dataType", "initialValues": { "valueCount": $elementCount, "value": 0 } },
+            "B": { "initialValuesDataType": "$dataType", "initialValues": { "valueCount": $elementCount, "value": 0 } },
+            "Out": { "initialValuesDataType": "$dataType", "initialValues": { "valueCount": $elementCount, "value": 0 } },
+            "Constants": 
+            {
+                "initialValuesDataType": "UNKNOWN",
+                "initialValues": 
+                [
+                    { "name": "elementCount", "type": "UINT32",  "value": $elementCount }
+                ],
+                "sizeInBytes": 256
+            }
+        },
+    
+        "dispatchables": 
+        {
+            "add": 
+            {
+                "type": "hlsl",
+                "sourcePath": "$hlslPath",
+                "compiler": "dxc",
+                "compilerArgs": 
+                [
+                    "-T", "cs_6_2",
+                    "-E", "CSMain",
+                    "-D", "NUM_THREADS=$numThreads",
+                    "-D", "T=$hlslType",
+                    "-enable-16bit-types"
+                ]
+            }
+        },
+    
+        "commands": 
+        [
+            {
+                "type": "dispatch",
+                "dispatchable": "add",
+                "threadGroupCount": $threadGroupCount,
+                "bindings": { "inputA": "A", "inputB": "B", "output": "Out", "constants": "Constants" }
+            }
+        ]
+    }
+"@
+}
+
 
 $template | out-file "temp.json"
 iex "..\build\win-x64\Release\dxdispatch.exe temp.json $ExtraArgs"
-rm temp.json
+# rm temp.json
