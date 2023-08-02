19425d61701ce5ca6896f2198843f68b7c27c57d
JacquesvanRhynMSFT <39970987+JacquesvanRhynMSFT@users.noreply.github.com>
2023-08-02 08:50:54 -0700

    added tabs to the Getting Started section (#489)
    * corrected capitalization of GitHub
    
    * added qualcomm's logo
    
    * added tabs to the Getting Started section

--- index.js
+++ index.js
@@ -28,6 +28,28 @@ xhttp.onreadystatechange =  function() {
         console.log(header_markup.textContent);
     }
 };
+
+$(document).ready(function() {
+  // Set the default tab as active
+  $(".tab-content").hide();
+  $("#tab1").show();
+  $(".tab-button[data-tab='tab1']").addClass("active focus");
+
+  // Add event listeners to all tab buttons
+  $(".tab-button").click(function() {
+    var tabName = $(this).attr("data-tab");
+
+    // Hide all tab content
+    $(".tab-content").hide();
+
+    // Remove "active" class from all tab buttons
+    $(".tab-button").removeClass("active");
+
+    // Show the clicked tab content and set the button as active
+    $("#" + tabName).show();
+    $(this).addClass("active focus");
+  });
+});
    
 xhttp.open("GET", "https://uhf.microsoft.com/en-US/shell/xml/MSDirectML?headerId=MSDirectMLHeader&footerid=MSDirectMLFooter&CookieComplianceEnabled=true", true);
 xhttp.send();
