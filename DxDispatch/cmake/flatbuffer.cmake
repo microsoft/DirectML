1b18481bf7a031172a60ec0aed48608dae10b88e
Sumit Agarwal <sumitagarwal@microsoft.com>
2024-06-17 17:10:48 -0700

    Add flatbuffer library

---
+++ DxDispatch/cmake/flatbuffer.cmake
@@ -0,0 +1,15 @@
+FetchContent_Declare(
+    flatbuffer
+    GIT_REPOSITORY https://github.com/google/flatbuffers.git
+    GIT_TAG        v23.5.26
+    GIT_SHALLOW    ON
+)
+
+FetchContent_GetProperties(flatbuffer)
+
+if(NOT flatbuffer_POPULATED)
+    FetchContent_Populate(flatbuffer)
+endif()
+
+add_library(flatbuffer INTERFACE)
+target_include_directories(rapidjson INTERFACE "${flatbuffer_SOURCE_DIR}/include")
