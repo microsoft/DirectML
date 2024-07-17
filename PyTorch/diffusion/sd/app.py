daf7ff8dee144ec1058b225683fc55373eee5f01
Sheil Kumar <smk2007@gmail.com>
2024-07-16 17:00:18 -0700

    Update PyTorch/diffusion/sd/app.py
    Co-authored-by: Dwayne Robinson <dwayner@microsoft.com>

--- PyTorch/diffusion/sd/app.py
+++ PyTorch/diffusion/sd/app.py
@@ -12,7 +12,7 @@ def preprocess(image):
     image = np.array(image).astype(np.float32) / 255.0
     image = image[None].transpose(0, 3, 1, 2)
     image = torch.from_numpy(image)
-    return 2.*image - 1.
+    return 2. * image - 1.
  
 lms = LMSDiscreteScheduler(
     beta_start=0.00085,
