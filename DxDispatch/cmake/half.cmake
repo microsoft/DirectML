6fc21042b1dd409fec4e9363e76b3716ca05fba2
Christian Larson <28911437+chrilaMSFT@users.noreply.github.com>
2024-08-16 10:00:37 -0700

    Add 3rd Party header Half to Source

--- DxDispatch/cmake/half.cmake
+++ DxDispatch/cmake/half.cmake
@@ -1,11 +1,7 @@
-FetchContent_Declare(
-    half
-    PREFIX half
-    URL "https://sourceforge.net/projects/half/files/half/2.1.0/half-2.1.0.zip/download"
-)
-FetchContent_MakeAvailable(half)
+# See ThirdPartyNotices.txt for details and attributions.
+# https://sourceforge.net/projects/half/files/half/2.1.0/half-2.1.0.zip/download
 
 add_library(half INTERFACE)
 add_library(Half::Half ALIAS half)
 
-target_include_directories(half INTERFACE "${half_SOURCE_DIR}/include")
+target_include_directories(half INTERFACE ${CMAKE_CURRENT_SOURCE_DIR}/ExternalDependencies/half-2.1.0)
